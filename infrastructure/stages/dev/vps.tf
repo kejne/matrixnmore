e6916d7b44af541f8d81a2f8aa39fa9a90741849
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2025-05-28 23:21:00 +0200

    test: setup an insecure poc

--- infrastructure/stages/dev/vps.tf
+++ infrastructure/stages/dev/vps.tf
@@ -1,9 +1,10 @@
 module "vps" {
   source = "../../modules/vps"
 
+  server_name = "matrixnmore"
   region      = "hel1"
   server_type = "cx22"
 
   hcloud_token = var.hcloud_token
-  ssh_public_key = file("../../.ssh/id.pub")
+  ssh_public_key = file("~/.ssh/id_ed25519.pub")
 }
